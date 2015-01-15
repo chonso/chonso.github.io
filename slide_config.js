e10721944bb877e5044a53c8619d2e7c5cd24cec
Chonso <chonso@gmail.com>
2015-01-15 01:45:51 -0600

    Comité de Seguimiento Académico
    1er Semestre

---
+++ slide_config.js
@@ -0,0 +1,35 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Universidad de Guanajuato',
+    subtitle: 'Maestría en Ciencias Aplicadas<br>Reunión de Comité de Seguimiento Académico',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/cgto_128.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Francisco Alonso Espinosa Chávez',
+   // company: 'Maestría en Ciencias Aplicadas',
+    gplus: 'http://plus.google.com/102860806290800670462',
+    //twitter: '@chonso_',
+    www: 'http://chonso.blogspot.mx',
+    github: 'http://github.com/chonso',
+    mail: 'chonso@gmail.com'
+  }
+  ]
+};
+
